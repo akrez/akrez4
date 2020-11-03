7b860ee101d7f1a75f7b8f56c0d9049c979ab8bc
akrez <akrez.like@gmail.com>
2020-11-04 01:39:33 +0330

    update Package model

--- models/Package.php
+++ models/Package.php
@@ -2,22 +2,32 @@
 
 namespace app\models;
 
+use Yii;
+use yii\db\ActiveQuery;
+
 /**
  * This is the model class for table "package".
  *
  * @property int $id
- * @property int $updated_at
+ * @property int|null $updated_at
  * @property string $status
  * @property double $price
  * @property string $guaranty
  * @property string $color
  * @property string $des
  * @property int $product_id
+ * @property string|null $user_name
  *
  * @property Product $product
+ * @property User $userName
  */
 class Package extends ActiveRecord
 {
+
+    public $guaranty;
+    public $color;
+    public $des;
+    //
     public $price_min;
     public $price_max;
 
@@ -29,20 +39,49 @@ class Package extends ActiveRecord
     public function rules()
     {
         return [
-            [['status'], 'in', 'range' => Status::getNormalKeys()],
+            [['status'], 'in', 'range' => array_keys(self::validStatuses())],
             [['color'], 'in', 'range' => array_keys(Color::getList())],
-            [['price', 'status', 'guaranty', '!product_id'], 'required'],
+            [['price', 'status', 'guaranty'], 'required'],
             [['price'], 'number'],
-            [['guaranty'], 'string', 'max' => 64],
-            [['des'], 'string', 'max' => 1023],
-            [['!blog_name'], 'required'],
+            [['guaranty', 'des'], 'safe'],
         ];
     }
 
-    public static function getActivePackagesQueryByCategories($activeCategories)
+    public static function validStatuses()
     {
-        $productQuery = Product::find()->select('id')->where(['status' => Status::STATUS_ACTIVE,])->andWhere(['category_id' => array_keys($activeCategories)]);
-        return Package::find()->where(['product_id' => $productQuery])->andWhere(['status' => Status::STATUS_ACTIVE]);
+        return [
+            Status::STATUS_ACTIVE => Yii::t('app', 'Active'),
+            Status::STATUS_DISABLE => Yii::t('app', 'Disable'),
+        ];
+    }
+
+    public static function userValidQuery($id = null)
+    {
+        $query = Package::find();
+        $query->andWhere(['status' => array_keys(Package::validStatuses())]);
+        $query->andWhere(['user_name' => Yii::$app->user->getId(),]);
+        $query->andFilterWhere(['id' => $id]);
+        return $query;
+    }
+
+    /**
+     * Gets query for [[Product]].
+     *
+     * @return ActiveQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::className(), ['id' => 'product_id']);
+    }
+
+    /**
+     * Gets query for [[UserName]].
+     *
+     * @return ActiveQuery
+     */
+    public function getUserName()
+    {
+        return $this->hasOne(User::className(), ['name' => 'user_name']);
     }
 
 }
