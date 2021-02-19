37cb7b66b23fb57a8c8fa94b982be37a7be04643
akrez <akrez.like@gmail.com>
2021-02-19 23:13:34 +0330

    some change in main page

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ p {text-align: justify;}
     <div class="body-content">
         <div class="row">
             <div class="col-lg-12">
-                <img src="<?= Yii::getAlias('@web/image/sample.jpg') ?>" class="img img-responsive img-rounded">
+                <img src="<?= Yii::getAlias('@web/image/sample.jpg') ?>" style="border-width: 1px;border-color: rgb(231,231,231);border-style: solid;" class="img img-responsive img-rounded">
             </div>
         </div>
     </div>
