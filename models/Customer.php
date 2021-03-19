b5bf0888666c5297e0a7c99b009b1d51f8610af9
akrez <akrez.like@gmail.com>
2021-03-19 23:04:22 +0330

    pass blogname

--- models/Customer.php
+++ models/Customer.php
@@ -12,16 +12,16 @@ use yii\web\IdentityInterface;
  *
  * @property int $id
  * @property string $status
- * @property string $created_at
- * @property string $updated_at
- * @property string $status
- * @property string $token
- * @property string $password_hash
- * @property string $reset_token
- * @property string $reset_at
- * @property string $email
- * @property string $mobile
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property string|null $token
+ * @property string|null $password_hash
+ * @property string|null $reset_token
+ * @property int|null $reset_at
+ * @property string|null $email
+ * @property string $blog_name
  *
+ * @property Blog $blogName
  */
 class Customer extends ActiveRecord implements IdentityInterface
 {
@@ -44,22 +44,26 @@ class Customer extends ActiveRecord implements IdentityInterface
 
         return [
             //signup
+            [['!blog_name',], 'required', 'on' => 'signup',],
             [['email',], 'required', 'on' => 'signup',],
             [['email',], 'unique', 'targetAttribute' => ['email', 'blog_name'], 'message' => Yii::t('yii', '{attribute} "{value}" has already been taken.'), 'on' => 'signup',],
             [['email',], 'email', 'on' => 'signup',],
             [['password',], 'required', 'on' => 'signup',],
             [['password',], 'minLenValidation', 'params' => ['min' => 6,], 'on' => 'signup',],
             //signin
+            [['!blog_name',], 'required', 'on' => 'signin',],
             [['email',], 'required', 'on' => 'signin',],
             [['email',], 'email', 'on' => 'signin',],
             [['password',], 'required', 'on' => 'signin',],
             [['password',], 'passwordValidation', 'on' => 'signin',],
             [['password',], 'minLenValidation', 'params' => ['min' => 6,], 'on' => 'signin',],
             //resetPasswordRequest
+            [['!blog_name',], 'required', 'on' => 'resetPasswordRequest',],
             [['email',], 'required', 'on' => 'resetPasswordRequest',],
             [['email',], 'findValidCustomerByEmailValidation', 'on' => 'resetPasswordRequest',],
             [['email',], 'email', 'on' => 'resetPasswordRequest',],
             //resetPassword
+            [['!blog_name',], 'required', 'on' => 'resetPassword',],
             [['email',], 'required', 'on' => 'resetPassword',],
             [['email',], 'findValidCustomerByEmailResetTokenValidation', 'on' => 'resetPassword',],
             [['email',], 'email', 'on' => 'resetPassword',],
@@ -101,7 +105,7 @@ class Customer extends ActiveRecord implements IdentityInterface
     public function passwordValidation($attribute, $params)
     {
         if (!$this->hasErrors()) {
-            $customer = Customer::findValidCustomerByEmail($this->email);
+            $customer = Customer::findValidCustomerByEmail($this->email, $this->blog_name);
             if ($customer && $customer->validatePassword($this->password)) {
                 return $this->_customer = $customer;
             }
@@ -113,7 +117,7 @@ class Customer extends ActiveRecord implements IdentityInterface
     public function findValidCustomerByEmailValidation($attribute, $params)
     {
         if (!$this->hasErrors()) {
-            $customer = Customer::findValidCustomerByEmail($this->email);
+            $customer = Customer::findValidCustomerByEmail($this->email, $this->blog_name);
             if ($customer) {
                 return $this->_customer = $customer;
             }
@@ -125,7 +129,7 @@ class Customer extends ActiveRecord implements IdentityInterface
     public function findValidCustomerByEmailResetTokenValidation($attribute, $params)
     {
         if (!$this->hasErrors()) {
-            $customer = Customer::findValidCustomerByEmailResetToken($this->email, $this->reset_token);
+            $customer = Customer::findValidCustomerByEmailResetToken($this->email, $this->reset_token, $this->blog_name);
             if ($customer) {
                 return $this->_customer = $customer;
             }
@@ -168,14 +172,24 @@ class Customer extends ActiveRecord implements IdentityInterface
         $this->reset_at = time();
     }
 
-    public static function findValidCustomerByEmail($email)
+    public static function findValidCustomerByEmail($email, $blogName)
     {
-        return self::find()->where(['status' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]])->andWhere(['blog_name' => Yii::$app->user->name()])->andWhere(['email' => $email])->one();
+        return self::find()
+            ->where(['status' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]])
+            ->andWhere(['blog_name' => $blogName])
+            ->andWhere(['email' => $email])
+            ->one();
     }
 
-    public static function findValidCustomerByEmailResetToken($email, $resetToken)
+    public static function findValidCustomerByEmailResetToken($email, $resetToken, $blogName)
     {
-        return self::find()->where(['status' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]])->andWhere(['email' => $email])->andWhere(['reset_token' => $resetToken])->andWhere(['>', 'reset_at', time() - self::TIMEOUT_RESET])->one();
+        return self::find()
+            ->where(['status' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]])
+            ->andWhere(['blog_name' => $blogName])
+            ->andWhere(['email' => $email])
+            ->andWhere(['reset_token' => $resetToken])
+            ->andWhere(['>', 'reset_at', time() - self::TIMEOUT_RESET])
+            ->one();
     }
 
     public static function generateResetToken()
@@ -206,26 +220,34 @@ class Customer extends ActiveRecord implements IdentityInterface
             'email' => $this->email,
             'status' => $this->status,
             'blog_name' => $this->blog_name,
-            'token' => ($this->getScenario() == 'signin' ? $this->token : null),
+            'token' => null,
         ];
     }
 
-    public function response()
+    public function toArrayWithToken(array $fields = [], array $expand = [], $recursive = true)
+    {
+        $array = $this->toArray($fields, $expand, $recursive);
+        $array['token'] = $this->token;
+        return $array;
+    }
+
+    public function response($includeToken = false)
     {
         return [
+            'customer' => $includeToken ? $this->toArrayWithToken() : $this->toArray(),
             'errors' => $this->errors,
         ];
     }
 
     /////
 
-    public static function signup($input)
+    public static function signup($input, $blogName)
     {
         try {
             $signup = new Customer(['scenario' => 'signup']);
             $signup->load($input, '');
             $signup->status = Status::STATUS_UNVERIFIED;
-            $signup->blog_name = Yii::$app->user->getIdentity()->name;
+            $signup->blog_name = $blogName;
             $signup->setAuthKey();
             $signup->setPasswordHash($signup->password);
             $signup->save();
@@ -235,11 +257,12 @@ class Customer extends ActiveRecord implements IdentityInterface
         }
     }
 
-    public static function signin($input)
+    public static function signin($input, $blogName)
     {
         try {
             $signin = new Customer(['scenario' => 'signin']);
             $signin->load($input, '');
+            $signin->blog_name = $blogName;
             $signin->validate();
             return $signin;
         } catch (Exception $e) {
@@ -258,11 +281,12 @@ class Customer extends ActiveRecord implements IdentityInterface
         }
     }
 
-    public static function resetPasswordRequest($input)
+    public static function resetPasswordRequest($input, $blogName)
     {
         try {
             $resetPasswordRequest = new Customer(['scenario' => 'resetPasswordRequest']);
             $resetPasswordRequest->load($input, '');
+            $resetPasswordRequest->blog_name = $blogName;
             if ($resetPasswordRequest->validate()) {
                 $blog = $resetPasswordRequest->getCustomer();
                 $blog->setResetToken();
@@ -278,11 +302,12 @@ class Customer extends ActiveRecord implements IdentityInterface
         }
     }
 
-    public static function resetPassword($input)
+    public static function resetPassword($input, $blogName)
     {
         try {
             $resetPassword = new Customer(['scenario' => 'resetPassword']);
             $resetPassword->load($input, '');
+            $resetPassword->blog_name = $blogName;
             if ($resetPassword->validate()) {
                 $blog = $resetPassword->getCustomer();
                 $blog->reset_token = null;
