2b7bbb0e5659dbd4f2fd4f78228199ffdc2c1cd5
akrez <akrez.like@gmail.com>
2021-04-08 20:09:53 +0430

    translate title

--- views/site/error.php
+++ views/site/error.php
@@ -2,12 +2,13 @@
 
 use yii\helpers\Html;
 
-$this->title = $name;
+$encodedMessage = Html::encode($message);
+$this->title = $encodedMessage;
 
 ?>
 
 
 <div class="jumbotron">
     <h1><?= Html::encode($exception->statusCode) ?></h1>
-    <p><?= nl2br(Html::encode($message)) ?></p>
+    <p><?= nl2br($encodedMessage) ?></p>
 </div>
