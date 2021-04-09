a124b0959bed7b683d471fa88e7a6068d2bb3db6
akrez <akrez.like@gmail.com>
2021-04-09 16:06:16 +0430

    favicon

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,7 +21,7 @@ $this->registerCss("
         <meta charset="<?= Yii::$app->charset ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= Yii::$app->view->registerLinkTag(['rel' => 'icon', 'href' => Yii::getAlias('@web/favicon.png')]) ?>
+        <link rel="icon" href="<?= Yii::getAlias('@web/favicon.svg') ?>" type="image/svg+xml">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
