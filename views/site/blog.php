1eced59eda2a4f915cb7467da3cd0d6f306c1920
akrez <akrez.like@gmail.com>
2021-04-11 20:55:34 +0430

    blog view

---
+++ views/site/blog.php
@@ -0,0 +1,150 @@
+<?php
+
+use app\assets\ChartJsAsset;
+use app\models\Blog;
+use app\models\Gallery;
+use yii\data\ActiveDataProvider;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii\web\View;
+
+ChartJsAsset::register($this);
+
+$this->title = Blog::print('title');
+
+$this->registerCss("
+    .direction-ltr {
+        direction: ltr;
+    }
+");
+
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="row">
+            <div class="col-sm-2 mt-2">
+                <?= Html::img((Blog::print('logo') ? Gallery::getImageUrl('logo', Blog::print('logo')) : '@web/cdn/image/logo.png'), ['class' => 'img img-responsive']); ?>
+            </div>
+            <div class="col-sm-10 mt-2">
+                <?= Html::tag('h3', Blog::print('title'), ['style' => 'margin-top: 0;']) ?>
+                <?= (Blog::print('slug') ? Html::tag('h4', Blog::print('slug')) : '') ?>
+                <?= (Blog::print('des') ? Html::tag('p', Blog::print('des'), ['style' => 'text-align: justify;']) : '') ?>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-sm-12">
+        <canvas id="canvas" height="85"></canvas>
+    </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="table-responsive">
+            <?=
+            GridView::widget([
+                'dataProvider' => $dataProvider,
+                'tableOptions' => ['class' => 'table table-bordered table-striped table-hover table-condensed'],
+                'columns' => [
+                    'id',
+                    'created_date',
+                    [
+                        'attribute' => 'user_agent',
+                        'contentOptions' => ['class' => 'direction-ltr'],
+                        'value' => function ($model) {
+                            return '';json_encode(get_browser($model->user_agent));
+                        }
+                    ],
+                    'ip',
+                    'action',
+                    [
+                        'attribute' => 'model_category_id',
+                        'value' => function ($model) use ($list) {
+                            if (isset($list['categories'][$model->model_category_id])) {
+                                return $list['categories'][$model->model_category_id];
+                            }
+                            return '';
+                        }
+                    ]
+                ],
+                'summary' => false,
+            ])
+            ?>
+        </div>
+    </div>
+</div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        let groupedDatas = <?= json_encode($groupedDatas) ?>;
+        let dates = <?= json_encode($dates) ?>;
+
+        let groupedDatasByDatas = {};
+        groupedDatas.forEach(element => {
+            if (!groupedDatasByDatas.hasOwnProperty(element.Ymd)) {
+                groupedDatasByDatas[element.Ymd] = 0;
+            }
+            groupedDatasByDatas[element.Ymd] += parseInt(element.count);
+        });
+
+        let datas = [];
+        dates.forEach(element => {
+            let count = 0;
+            if (groupedDatasByDatas.hasOwnProperty(element)) {
+                count = groupedDatasByDatas[element];
+            }
+            datas.push(count);
+        });
+
+        var config = {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'بازدید',
+                    fill: true,
+                    borderColor: '#993799',
+                    backgroundColor: 'rgba(209,165,209,0.25)',
+                    spanGaps: true,
+                    data: datas
+                }]
+            },
+            options: {
+                responsive: true,
+                legend: false,
+                title: {
+                    display: true,
+                    text: 'تعداد بازدید'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                    rtl: true
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            maxRotation: 90,
+                            minRotation: 90
+                        }
+                    }]
+                }
+            },
+            defaultFontFamily: Chart.defaults.global.defaultFontFamily = "'Sahel'",
+        };
+        var ctx = document.getElementById('canvas').getContext('2d');
+        new Chart(ctx, config);
+    });
+</script>
