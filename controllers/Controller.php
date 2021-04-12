5c226836a0cc31735674846d75cc33c8924b1ee2
akrez <akrez.like@gmail.com>
2021-04-12 22:48:03 +0430

    log in init

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -4,16 +4,19 @@ namespace app\controllers;
 
 use app\models\LogAdmin;
 use Yii;
+use yii\base\Application;
 use yii\helpers\Url;
 use yii\web\Controller as BaseController;
 use yii\web\ForbiddenHttpException;
 
 class Controller extends BaseController
 {
-    public function afterAction($action, $result)
+    public function init()
     {
-        @LogAdmin::log();
-        return parent::afterAction($action, $result);
+        parent::init();
+        Yii::$app->on(Application::EVENT_AFTER_REQUEST, function ($event) {
+            @LogAdmin::log();
+        });
     }
 
     public function defaultBehaviors($rules)
@@ -24,7 +27,7 @@ class Controller extends BaseController
             'rules' => $rules,
             'denyCallback' => function ($rule, $action) {
                 if (Yii::$app->user->isGuest) {
-                    @LogAdmin::log(['response_http_code' => 403]);
+                    @LogAdmin::setData(['response_http_code' => 403]);
                     Yii::$app->user->setReturnUrl(Url::current());
                     return $this->redirect(['/site/signin']);
                 }
