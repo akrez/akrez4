2afb9d47cd10e81cc220239885fbefb2ad569bfa
AliAkbar <akrez@akrez.com>
2021-06-17 02:08:53 +0430

    TelegramContenttransition

---
+++ models/TelegramContenttransition.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "telegram_webhook".
+ *
+ * @property int $id
+ * @property int|null $created_at
+ * @property string|null $forward_from
+ * @property string|null $update_id
+ * @property string|null $message
+ */
+class TelegramContenttransition  extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'telegram_webhook';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['message'], 'string'],
+            [['forward_from'], 'string', 'max' => 31],
+            [['update_id'], 'string', 'max' => 15],
+        ];
+    }
+
+    public static function singleSave($message)
+    {
+        $model = new TelegramContenttransition();
+        $model->message = $message;
+        $model->save();
+    }
+}
