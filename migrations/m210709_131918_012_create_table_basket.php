ffc38c1b58a2b817c06d070b999871fddfd75a33
AliAkbar <akrez@akrez.com>
2021-07-15 02:30:17 +0430

    basket

--- migrations/m210709_131918_012_create_table_basket.php
+++ migrations/m210709_131918_012_create_table_basket.php
@@ -20,29 +20,36 @@ class m210709_131918_012_create_table_basket extends Migration
             [
                 'id' => $this->primaryKey(),
                 'updated_at' => $this->integer(),
-                'status' => $this->string(12)->notNull(),
+                'created_at' => $this->integer(),
                 'price' => $this->double()->notNull(),
                 'cnt' => $this->integer()->notNull(),
-                'product_id' => $this->integer()->notNull(),
                 'package_id' => $this->integer(),
-                'customer_id' => $this->integer()->notNull(),
+                'customer_id' => $this->integer(),
                 'blog_name' => $this->string(31),
             ],
             $tableOptions
         );
 
         $this->createIndex('package_id', '{{%basket}}', ['package_id']);
-        $this->createIndex('product_id', '{{%basket}}', ['product_id']);
         $this->createIndex('blog_name', '{{%basket}}', ['blog_name']);
         $this->createIndex('customer_id', '{{%basket}}', ['customer_id']);
 
         $this->addForeignKey(
-            'basket_ibfk_1',
+            'basket_ibfk_3',
             '{{%basket}}',
-            ['product_id'],
-            '{{%product}}',
+            ['package_id'],
+            '{{%package}}',
             ['id'],
-            'CASCADE',
+            'SET NULL',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'basket_ibfk_4',
+            '{{%basket}}',
+            ['customer_id'],
+            '{{%customer}}',
+            ['id'],
+            'SET NULL',
             'CASCADE'
         );
         $this->addForeignKey(
