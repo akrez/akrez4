ba762664d1f25f000ce84a5ed65835c48b944fa2
AliAkbar <akrez@akrez.com>
2021-07-30 00:52:18 +0430

    iso 1366-2

--- models/Province.php
+++ models/Province.php
@@ -2,43 +2,45 @@
 
 namespace app\models;
 
+use Yii;
+
 class Province extends Model
 {
 
     public static function getList()
     {
         return [
-            'tehran' => 'تهران',
-            'khuzestan' => 'خوزستان',
-            'esfahan' => 'اصفهان',
-            'khorasan_razavi' => 'خراسان رضوی',
-            'fars' => 'فارس',
-            'bushehr' => 'بوشهر',
-            'mazandaran' => 'مازندران',
-            'alborz' => 'البرز',
-            'east_azarbaijan' => 'آذربایجان شرقی',
-            'kerman' => 'كرمان',
-            'markazi' => 'مرکزی',
-            'gilan' => 'گیلان',
-            'western_azerbaijan' => 'آذربایجان غربی',
-            'hormozgan' => 'هرمزگان',
-            'yazd' => 'یزد',
-            'qazvin' => 'قزوین',
-            'kermanshah' => 'کرمانشاه',
-            'hamedan' => 'همدان',
-            'sistan_and_baluchistan' => 'سیستان و بلوچستان',
-            'golestan' => 'گلستان',
-            'kohgiloyeh_boyerahmad' => 'كهگیلویه و بویراحمد',
-            'lorestan' => 'لرستان',
-            'qom' => 'قم',
-            'zanjan' => 'زنجان',
-            'ardabil' => 'اردبیل',
-            'kurdistan' => 'كردستان',
-            'semnan' => 'سمنان',
-            'chaharmahal_and_bakhtiari' => 'چهارمحال و بختیاری',
-            'ilam' => 'ایلام',
-            'north_khorasan' => 'خراسان شمالی',
-            'south_khorasan' => 'خراسان جنوبی',
+            'IR-01' => Yii::t("app", "Azarbayjan-e Sharqi"),
+            'IR-02' => Yii::t("app", "Azarbayjan-e Gharbi"),
+            'IR-03' => Yii::t("app", "Ardabil"),
+            'IR-04' => Yii::t("app", "Esfahan"),
+            'IR-05' => Yii::t("app", "Ilam"),
+            'IR-06' => Yii::t("app", "Bushehr"),
+            'IR-07' => Yii::t("app", "Tehran"),
+            'IR-08' => Yii::t("app", "Chahar Maal va Bakhtiari"),
+            'IR-10' => Yii::t("app", "Khuzestan"),
+            'IR-11' => Yii::t("app", "Zanjan"),
+            'IR-12' => Yii::t("app", "Semnan"),
+            'IR-13' => Yii::t("app", "Sistan va Baluchestan"),
+            'IR-14' => Yii::t("app", "Fars"),
+            'IR-15' => Yii::t("app", "Kerman"),
+            'IR-16' => Yii::t("app", "Kordestan"),
+            'IR-17' => Yii::t("app", "Kermanshah"),
+            'IR-18' => Yii::t("app", "Kohgiluyeh va Bowyer Amad"),
+            'IR-19' => Yii::t("app", "Gilan"),
+            'IR-20' => Yii::t("app", "Lorestan"),
+            'IR-21' => Yii::t("app", "Mazandaran"),
+            'IR-22' => Yii::t("app", "Markazi"),
+            'IR-23' => Yii::t("app", "Hormozgan"),
+            'IR-24' => Yii::t("app", "Hamadan"),
+            'IR-25' => Yii::t("app", "Yazd"),
+            'IR-26' => Yii::t("app", "Qom"),
+            'IR-27' => Yii::t("app", "Golestan"),
+            'IR-28' => Yii::t("app", "Qazvin"),
+            'IR-29' => Yii::t("app", "Khorasan-e Jonubi"),
+            'IR-30' => Yii::t("app", "Khorasan-e Razavi"),
+            'IR-31' => Yii::t("app", "Khorasan-e Shomali"),
+            'IR-32' => Yii::t("app", "Alborz"),
         ];
     }
 
@@ -47,5 +49,4 @@ class Province extends Model
         $list = self::getList();
         return (isset($list[$item]) ? $list[$item] : null);
     }
-
 }
